bb8b0de7e62cee3b22298b02ee1446b3b445200e
Pratham Das <prathamdas003@gmail.com>
2024-08-22 02:25:19 +0530

    chore(app): shadcn added

--- client/src/app/pages/HomePage.tsx
+++ client/src/app/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function HomePage() {
   return (
     <main>
       <h1>Hello world from home page</h1>
+      <Button variant="ghost">Test button</Button>
     </main>
   )
 }
